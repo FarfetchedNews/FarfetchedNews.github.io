68f2418371958a3c93a844c66c7069f5938612f5
Zach <34075156+zkeepah@users.noreply.github.com>
2020-06-19 17:03:56 -0500

    Update weather.js

--- Scripts/weather.js
+++ Scripts/weather.js
@@ -4,9 +4,10 @@ var test;
 xhttp.onreadystatechange = function() {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
 			var arrayWithValues = xhttp.responseText.trim().split('\t');
-			var i;
-			for (i = 0; i<arrayWithValues.length; i++) {
-				arrayWithValues.split[i]('\n'); }
+			weatherString = arrayWithValues.tostring();
+			arrayWithValues = weatherString.split('\n');
+			weatherString = arrayWithValues.tostring();
+			arrayWithValues = weatherString.split('',);
 			console.log(arrayWithValues);
 			document.getElementById('test').innerText = arrayWithValues[2];
         };
