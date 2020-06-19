86828f44f5d4e9e3ff33ce7dd3924ecce565310a
Zach <34075156+zkeepah@users.noreply.github.com>
2020-06-19 04:32:12 -0500

    a

--- Scripts/weather.js
+++ Scripts/weather.js
@@ -1,10 +1,10 @@
-sunday = d3.tsvParse("/Scripts/selectedweather.tsv",[1]);
-monday = d3.tsvParse("/Scripts/selectedweather.tsv",[2]);
-tuesday = d3.tsvParse("/Scripts/selectedweather.tsv",[3]);
-wednesday = d3.tsvParse("/Scripts/selectedweather.tsv",[4]);
-thursday = d3.tsvParse("/Scripts/selectedweather.tsv",[5]);
-friday = d3.tsvParse("/Scripts/selectedweather.tsv",[6]);
-saturday = d3.tsvParse("/Scripts/selectedweather.tsv",[7]);
+sunday = d3.tsvParseRows("/Scripts/selectedweather.tsv",[1]);
+monday = d3.tsvParseRows("/Scripts/selectedweather.tsv",[2]);
+tuesday = d3.tsvParseRows("/Scripts/selectedweather.tsv",[3]);
+wednesday = d3.tsvParseRows("/Scripts/selectedweather.tsv",[4]);
+thursday = d3.tsvParseRows("/Scripts/selectedweather.tsv",[5]);
+friday = d3.tsvParseRows("/Scripts/selectedweather.tsv",[6]);
+saturday = d3.tsvParseRows("/Scripts/selectedweather.tsv",[7]);
 
 var element = document.getElementById("test");
 element.textContent = "sunday";
