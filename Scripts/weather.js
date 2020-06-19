cdc14adb5241979ff84b8e29b52f2ae025e071cf
Zach <34075156+zkeepah@users.noreply.github.com>
2020-06-19 15:10:40 -0500

    Update weather.js

--- Scripts/weather.js
+++ Scripts/weather.js
@@ -1,12 +1,10 @@
 var test;
 
 d3.tsvParseRows("/Scripts/selectedweather.tsv", function(data) {
-	console.log(data[0].temp);
-	console.log(data[0].Text);
-	data.forEach(function(d) {
-	d.temp = +d.temp;
-	test = toString(d.Text);
-	})
+	for (var i = 0; i < data.length; i++) {
+		console.log(data[i].temp);
+		console.log(data[i].Text);
+	}
 });
 
 document.getElementById("test").textContent = test;
