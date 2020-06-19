f2ea7b3603ccfe2eee88ecad75731300597dff04
Zach <34075156+zkeepah@users.noreply.github.com>
2020-06-19 03:43:56 -0500

    some fiddling with weather,js

---
+++ Scripts/weather.js
@@ -0,0 +1,10 @@
+d3.tsv("/Scripts/selectedweather.tsv",[1]) = sunday;
+d3.tsv("/Scripts/selectedweather.tsv",[2]) = monday;
+d3.tsv("/Scripts/selectedweather.tsv",[3]) = tuesday;
+d3.tsv("/Scripts/selectedweather.tsv",[4]) = wednesday;
+d3.tsv("/Scripts/selectedweather.tsv",[5]) = thursday;
+d3.tsv("/Scripts/selectedweather.tsv",[6]) = friday;
+d3.tsv("/Scripts/selectedweather.tsv",[7]) = saturday;
+
+var element = document.getElementById("test");
+element.innerHTML = "sunday";
