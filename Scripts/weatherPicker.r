b2f54d6711de6986b74ac315a15d1651bc0fd0a4
Zach <34075156+zkeepah@users.noreply.github.com>
2020-06-19 18:34:17 -0500

    commented out test code

--- Scripts/weatherPicker.r
+++ Scripts/weatherPicker.r
@@ -11,6 +11,6 @@ sample_n(weatherTable, 7) -> randomisedWeather
 randomisedWeather <- cbind(temp = rownames(randomisedWeather), randomisedWeather)
 rownames(randomisedWeather) <- c()
 
-print(randomisedWeather[2])
+#print(randomisedWeather[2])
 
 write.table (randomisedWeather, file = "selectedweather.tsv", sep = '\t', quote = FALSE, na = "MISSING VALUE", row.names = FALSE)
