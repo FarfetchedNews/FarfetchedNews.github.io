4795918184b173fbb8eb0e4623ca869e1422b757
Zach <34075156+zkeepah@users.noreply.github.com>
2020-06-19 05:11:54 -0500

    a

--- Scripts/README.txt
+++ Scripts/README.txt
@@ -4,4 +4,4 @@ The first row is header to lable the two columns
 I have created a script using R that will pull 5 random lines from the tsv and write them to a document
 The R script will import the words.tsv, weather.tsv, quotes.tsv, and books.tsv files, pull random lines from them, and then write them to a file
 
-However, it renders the data to a csv(for now)
+After spending several hours trying to get the js to work, I'm abandoning it temporarily so I can sleep. I'll see if I can figure out what I'm doing when I awaken
--- Scripts/weather.js
+++ Scripts/weather.js
@@ -1,10 +1,6 @@
-sunday = d3.tsvParseRows("/Scripts/selectedweather.tsv",[1]);
-monday = d3.tsvParseRows("/Scripts/selectedweather.tsv",[2]);
-tuesday = d3.tsvParseRows("/Scripts/selectedweather.tsv",[3]);
-wednesday = d3.tsvParseRows("/Scripts/selectedweather.tsv",[4]);
-thursday = d3.tsvParseRows("/Scripts/selectedweather.tsv",[5]);
-friday = d3.tsvParseRows("/Scripts/selectedweather.tsv",[6]);
-saturday = d3.tsvParseRows("/Scripts/selectedweather.tsv",[7]);
+d3.tsv(selectedweather.tsv, function(data) {
+	console.log(data)
+});
 
 var element = document.getElementById("test");
 element.textContent = "sunday";
