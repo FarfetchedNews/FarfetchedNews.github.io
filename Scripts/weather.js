9108db28b3a063903a96d5b0169278f195568469
Zach <34075156+zkeepah@users.noreply.github.com>
2020-06-19 06:08:23 -0500

    Update weather.js

--- Scripts/weather.js
+++ Scripts/weather.js
@@ -3,5 +3,11 @@ d3.tsv("/Scripts/selectedweather.tsv", function(data) {
 	test = data[1];
 });
 
+d3.tsv("selectedweather.tsv", function(data) {
+	console.log(data);
+	test = data[1];
+});
+
+
 var element = document.getElementById("test");
 element.textContent = test;
