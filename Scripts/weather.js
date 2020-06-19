fa23db65f50ea5b82a5f9c9c8ef68866fc9d4737
Zach <34075156+zkeepah@users.noreply.github.com>
2020-06-19 17:20:23 -0500

    implemented automatic updating of temperatures on index.html from temperatures in selectedweather.tsv

--- Scripts/weather.js
+++ Scripts/weather.js
@@ -3,11 +3,19 @@ var test;
 
 xhttp.onreadystatechange = function() {
 	if (xhttp.readyState == 4 && xhttp.status == 200) {
-		var arrayWithValues = xhttp.responseText.trim().split('\t');
-		weatherString = arrayWithValues.join('\n');
-		arrayWithValues = weatherString.split('\n');
-		console.log(arrayWithValues);
-		document.getElementById('test').innerText = arrayWithValues[2];
+		var arrayWithValues = xhttp.responseText.trim().split('\t'); //splits string at tab to array
+		weatherString = arrayWithValues.join('\n'); //joins the array to a string using new lines to separate
+		arrayWithValues = weatherString.split('\n'); //splits the string into an array at the new lines
+	//	console.log(arrayWithValues);
+	
+		//set temps
+		document.getElementById('suntemp').innerText = arrayWithValues[2]; //Start at index two because headers take up the first two indices
+		document.getElementById('montemp').innerText = arrayWithValues[4];
+		document.getElementById('tuetemp').innerText = arrayWithValues[6];
+		document.getElementById('wedtemp').innerText = arrayWithValues[8];
+		document.getElementById('thutemp').innerText = arrayWithValues[10];
+		document.getElementById('fritemp').innerText = arrayWithValues[12];
+		document.getElementById('sattemp').innerText = arrayWithValues[14];
 	};
 };
 
