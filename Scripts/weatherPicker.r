976280939773af6876f7d50e0ac46a8bdc7e3885
Zach <34075156+zkeepah@users.noreply.github.com>
2020-06-19 06:00:07 -0500

    guh

--- Scripts/weatherPicker.r
+++ Scripts/weatherPicker.r
@@ -4,9 +4,15 @@ library(dplyr)
 
 # Weather stuffs ----------------------------------------------------------
 
-read.table(file = 'weather.tsv', sep = '\t', row.names = 1, header = FALSE) -> weatherTable
+read.table(file = 'weather.tsv', sep = '\t', row.names = 1, header = TRUE) -> weatherTable
 
 sample_n(weatherTable, 7) -> randomisedWeather
+
+randomisedWeather <- cbind(Row.Names = rownames(randomisedWeather), randomisedWeather)
+rownames(randomisedWeather) <- NULL
+
+colnames(randomisedWeather)[1] -> temp
+colnames(randomisedWeather)[2] -> text
 
 print(randomisedWeather)
 
