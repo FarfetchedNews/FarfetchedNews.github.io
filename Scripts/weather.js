30fdcc482bcdbce9a91ec317e28d93a015a23b8f
Zach <34075156+zkeepah@users.noreply.github.com>
2020-06-19 15:14:35 -0500

    Update weather.js

--- Scripts/weather.js
+++ Scripts/weather.js
@@ -1,6 +1,6 @@
 var test;
 
-d3.tsvParseRows("/Scripts/selectedweather.tsv", function(data) {
+d3.tsv("/Scripts/selectedweather.tsv", function(data) {
 	for (var i = 0; i < data.length; i++) {
 		console.log(data[i].temp);
 		console.log(data[i].Text);
