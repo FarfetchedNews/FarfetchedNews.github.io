38bfb746de2f1a7e157c5f17141a8a596049d7f4
Zach <34075156+zkeepah@users.noreply.github.com>
2020-06-19 17:24:20 -0500

    completed implementation of weather updating function

--- Scripts/weather.js
+++ Scripts/weather.js
@@ -16,6 +16,15 @@ xhttp.onreadystatechange = function() {
 		document.getElementById('thutemp').innerText = arrayWithValues[10];
 		document.getElementById('fritemp').innerText = arrayWithValues[12];
 		document.getElementById('sattemp').innerText = arrayWithValues[14];
+		
+		//setfx
+		document.getElementById('sunfx').innerText = arrayWithValues[3]; //Start at index three because headers take up the first two indices
+		document.getElementById('monfx').innerText = arrayWithValues[5];
+		document.getElementById('tuefx').innerText = arrayWithValues[7];
+		document.getElementById('wedfx').innerText = arrayWithValues[9];
+		document.getElementById('thufx').innerText = arrayWithValues[11];
+		document.getElementById('frifx').innerText = arrayWithValues[13];
+		document.getElementById('satfx').innerText = arrayWithValues[15];
 	};
 };
 
