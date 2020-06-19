689afbc49bbea322291193ef31f0f584293bd9d4
Zach <34075156+zkeepah@users.noreply.github.com>
2020-06-19 14:23:55 -0500

    Update weather.js

--- Scripts/weather.js
+++ Scripts/weather.js
@@ -1,15 +1,6 @@
-var files = ["/Scripts/selectedweather.tsv"]; //Have no idea if this is necessary or does anything
-
-var promises = []; //stackoverflow says to do this
-
-//Stackoverflow said to write this too
-Promise.all(promises).then(function(values) {
-    console.log(values)
-});
-
 d3.tsv("/Scripts/selectedweather.tsv", function(data) {
-	console.log(data[1]);
-	test = data[1];
+	console.log(data[0]);
+	test = data[0];
 	
 });
 
