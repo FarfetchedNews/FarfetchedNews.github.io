67279e3e5b41ca3182ff3fde65c4f9b4f4b496df
Zach <34075156+zkeepah@users.noreply.github.com>
2020-06-19 03:56:43 -0500

    resolved an error

--- Scripts/weather.js
+++ Scripts/weather.js
@@ -1,10 +1,10 @@
-d3.tsv("/Scripts/selectedweather.tsv",[1]) = sunday;
-d3.tsv("/Scripts/selectedweather.tsv",[2]) = monday;
-d3.tsv("/Scripts/selectedweather.tsv",[3]) = tuesday;
-d3.tsv("/Scripts/selectedweather.tsv",[4]) = wednesday;
-d3.tsv("/Scripts/selectedweather.tsv",[5]) = thursday;
-d3.tsv("/Scripts/selectedweather.tsv",[6]) = friday;
-d3.tsv("/Scripts/selectedweather.tsv",[7]) = saturday;
+sunday = d3.tsv("/Scripts/selectedweather.tsv",[1]);
+monday = d3.tsv("/Scripts/selectedweather.tsv",[2]);
+tuesday = d3.tsv("/Scripts/selectedweather.tsv",[3]);
+wednesday = d3.tsv("/Scripts/selectedweather.tsv",[4]);
+thursday = d3.tsv("/Scripts/selectedweather.tsv",[5]);
+friday = d3.tsv("/Scripts/selectedweather.tsv",[6]);
+saturday = d3.tsv("/Scripts/selectedweather.tsv",[7]);
 
 var element = document.getElementById("test");
-element.innerHTML = "sunday";
+element.textContent = "sunday";
