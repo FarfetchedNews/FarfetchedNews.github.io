893c7467fd36ac5c3b5541672d8e02cc1b39e3c9
Zach <34075156+zkeepah@users.noreply.github.com>
2020-06-19 17:06:59 -0500

    Update weather.js

--- Scripts/weather.js
+++ Scripts/weather.js
@@ -2,15 +2,13 @@ var xhttp = new XMLHttpRequest();
 var test;
 
 xhttp.onreadystatechange = function() {
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
-			var arrayWithValues = xhttp.responseText.trim().split('\t');
-			weatherString = arrayWithValues.tostring();
-			arrayWithValues = weatherString.split('\n');
-			weatherString = arrayWithValues.tostring();
-			arrayWithValues = weatherString.split('',);
-			console.log(arrayWithValues);
-			document.getElementById('test').innerText = arrayWithValues[2];
-        };
+	if (xhttp.readyState == 4 && xhttp.status == 200) {
+		var arrayWithValues = xhttp.responseText.trim().split('\t');
+		weatherString = arrayWithValues.join('\n');
+		arrayWithValues = weatherString.split('\n');
+		console.log(arrayWithValues);
+		document.getElementById('test').innerText = arrayWithValues[2];
+	};
 };
 
 xhttp.open("GET", "/Scripts/selectedweather.tsv", true);
