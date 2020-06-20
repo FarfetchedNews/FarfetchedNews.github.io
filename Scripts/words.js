77197f24c33f8679a6cacfcb044011fcc3f3a54c
Zach <34075156+zkeepah@users.noreply.github.com>
2020-06-19 21:13:03 -0500

    implemented js for word of the month

---
+++ Scripts/words.js
@@ -0,0 +1,21 @@
+var xhttp = new XMLHttpRequest();
+var test;
+
+xhttp.onreadystatechange = function() {
+	if (xhttp.readyState == 4 && xhttp.status == 200) {
+		var wordarray = xhttp.responseText.trim().split('\t'); //splits string at tab to array
+		wordString = wordarray.join('\n'); //joins the array to a string using new lines to separate
+		wordarray = wordString.split('\n'); //splits the string into an array at the new lines
+
+	//	console.log(wordarray);
+	
+		//set word
+		document.getElementById('wordMonthWord').innerText = wordarray[2]; //Start at index two because headers take up the first two indices
+		document.getElementById('wordMonthDef').innerText = wordarray[3];
+		document.getElementById('wordMonthEx').innerText = wordarray[4];
+	};
+};
+
+xhttp.open("GET", "/Scripts/selectedweather.tsv", true);
+
+xhttp.send();
