b1d2547ab28b25ff406af7edba88f7feca80a433
Zach <34075156+zkeepah@users.noreply.github.com>
2020-06-19 21:43:11 -0500

    implemented js for quote of the month

---
+++ Scripts/quotes.js
@@ -0,0 +1,20 @@
+var xhttp = new XMLHttpRequest();
+var test;
+
+xhttp.onreadystatechange = function() {
+	if (xhttp.readyState == 4 && xhttp.status == 200) {
+		var quotearray = xhttp.responseText.trim().split('\t'); //splits string at tab to array
+		quoteString = quotearray.join('\n'); //joins the array to a string using new lines to separate
+		quotearray = quoteString.split('\n'); //splits the string into an array at the new lines
+
+	//	console.log(wordarray);
+	
+		//set word
+		document.getElementById('quoteMonthQuote').innerText = quotearray[2]; //Start at index two because headers take up the first two indices
+		document.getElementById('quoteMonthSayer').innerText = quotearray[3];
+	};
+};
+
+xhttp.open("GET", "/Scripts/quoteofthemonth.tsv", true);
+
+xhttp.send();
