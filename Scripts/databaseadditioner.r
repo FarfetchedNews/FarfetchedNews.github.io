da15ba5d66e01a624c161db6a4cb2e1f954c116f
zkeepah <zkeepah@gmail.com>
2020-06-20 21:32:02 -0500

    declared some variables in r

---
+++ Scripts/databaseadditioner.r
@@ -0,0 +1,17 @@
+
+# Variables ---------------------------------------------------------------
+
+quotesSelected -> FALSE
+booksSelected -> FALSE
+wordsSelected -> FALSE
+weatherSelected -> FALSE
+
+newTemp -> NULL
+newFx -> NULL
+newSayer -> NULL
+newQuote -> NULL
+newWord -> NULL
+newDef -> NULL
+newEX -> NULL
+newBook -> NULL
+newAuthor -> NULL
